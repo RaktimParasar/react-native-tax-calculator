1732376ca4d242dbf162975527094e49472a801d
Raktim Parasar <raktimparasar96@gmail.com>
2023-04-18 12:45:28 +0530

    adding theme & input field comp

---
+++ app/components/Inputfield/InputField.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const InputField = () => {
+  return (
+    <View>
+      <Text>InputField</Text>
+    </View>
+  );
+};
+
+export default InputField;
--- app/index.js
+++ app/index.js
@@ -1,18 +1,22 @@
 import { SafeAreaView, Text, View, ScrollView } from "react-native";
 import { Stack } from "expo-router";
+import { COLORS, FONT } from "../constant/theme";
+import InputField from "./components/Inputfield/InputField";
 
 const Home = () => {
   return (
     <SafeAreaView
       style={{
         flex: 1,
+        backgroundColor: COLORS.lightWhite,
       }}
     >
       <Stack.Screen
         options={{
-          headerStyle: {},
+          headerStyle: { backgroundColor: COLORS.primary },
           headerShadowVisible: false,
-          headerTitle: "",
+          headerTitle: "Income Tax Calculator",
+          headerTitleStyle: { color: COLORS.white, fontFamily: FONT.bold },
         }}
       />
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -23,6 +27,7 @@ const Home = () => {
         >
           <Text>Hello, World 🍀</Text>
         </View>
+        <InputField />
       </ScrollView>
     </SafeAreaView>
   );
